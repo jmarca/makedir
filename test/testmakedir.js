1885edddf35051419f48ff0871fa558c99dc8ca9
James E. Marca <jmarca@translab.its.uci.edu>
2014-03-20 17:51:30 -0700

    initial set of mocha-ified testing done

--- test/testmakedir.js
+++ test/testmakedir.js
@@ -1,34 +1,91 @@
-var _ = require('underscore')._;
-var fs = require('fs');
+/*global require __dirname describe it after */
+var should = require('should')
+var fs = require('fs')
+var async = require('async')
 
-var dirs = require('../lib/makedir');
+var makedir = require('../.')
 
 // A lot of paths, and repeats, so that you have to handle multiple
 // simultaneous makes
 
-var paths = ['/tmp/repos/jem/node-data-proxy/public/data/airbasins/monthly/2007'
-            ,'/tmp/pos/jem/node-data-proxy/public/data/airbasins/monthly/2007'
-            ,'/tmp/os/jem/node-data-proxy/public/data/airbasins/monthly/2007'
-            ,'/tmp/s/jem/node-data-proxy/public/data/airbasins/monthly/2007'
-            ,'/tmp/repos/jem/node-data-proxy/public/data/airbasins/monthly/2007'
-            ,'/tmp/pos/jem/node-data-proxy/public/data/airbasins/monthly/2007'
-            ,'/tmp/os/jem/node-data-proxy/public/data/airbasins/monthly/2007'
-            ,'/tmp/s/jem/node-data-proxy/public/data/airbasins/monthly/2007'
+var path    = require('path')
+var rootdir = path.normalize(__dirname)
+
+var path_components = [rootdir,'foo','bar','baz','bat']
+
+var paths = [rootdir + '/foo/bar/baz/bat'
+            ,rootdir + '/repos/jem/node-data-proxy/public/data/airbasins/monthly/2007'
+            ,rootdir + '/pos/jem/node-data-proxy/public/data/airbasins/monthly/2007'
+            ,rootdir + '/os/jem/node-data-proxy/public/data/airbasins/monthly/2007'
+            ,rootdir + '/s/jem/node-data-proxy/public/data/airbasins/monthly/2007'
+            ,rootdir + '/repos/jem/node-data-proxy/public/data/airbasins/monthly/2007'
+            ,rootdir + '/pos/jem/node-data-proxy/public/data/airbasins/monthly/2007'
+            ,rootdir + '/os/jem/node-data-proxy/public/data/airbasins/monthly/2007'
+            ,rootdir + '/s/jem/node-data-proxy/public/data/airbasins/monthly/2007'
             ];
-_.each(paths
-      ,function(p){
-          dirs.makedir(p
-                      ,function (path){
-                          return function(err){
-                              if(err){
-                                  console.log('error making'+ path+ ' :'+ JSON.stringify(err));
-                              }else{
-                                  console.log('made'+ path) ;
-                              }
-                          };
-                      }(p));
-          return 1;
-      });
+
+after(function(done){
+    try{
+        fs.unlinkSync(path_components.join('/'))
+    } catch (x) {
+        // whatever
+    }
+    paths.forEach(function(path){
+        try{
+            fs.unlinkSync(path)
+        } catch (x) {
+            // whatever
+        }
+        return null
+
+    });
 
 
+    return done()
+})
 
+describe('create directories',function(){
+    it('should create a directory and its parent',function(done){
+        makedir.makedir(path_components.join('/'),function(err){
+            should.not.exist(err)
+            // make all of the pieces exist
+            var path
+            for(var i = 0; i < path_components.length; i++){
+
+                path = path_components.slice(0,i+1)
+                var stats = fs.statSync(path.join('/'))
+                stats.isDirectory().should.be.ok
+
+            }
+
+
+            return done()
+        })
+    })
+
+    it('should create many directories at once',function(done){
+        async.each(paths
+                  ,function(p,cb){
+                       makedir.makedir(p,function(err){
+                           should.not.exist(err)
+                           return cb()
+                       })
+                       return null
+                   }
+                  ,function(e){
+                       should.not.exist(e)
+                       // make sure all of the paths exist
+
+                       paths.forEach(function(p){
+
+                           var stats = fs.statSync(p)
+                           stats.isDirectory().should.be.ok
+
+                       })
+                       return done()
+                   });
+        return null
+
+    })
+
+})
